a2755e0847657914b22e63b573fa260970ac179a
PhillUndSoos <ph_hass@web.de>
2023-05-11 15:18:14 +0200

    Basic Filestructure and media

---
+++ the-ripoff-in-question/js/script.js
@@ -0,0 +1,8 @@
+/** @type { HTMLCanvasElement } 
+ *  +++ PNG DIMENSION +++
+ *  ./media/images/berd.png:    1308x177 | 218 WDITH per frame (6)
+ *  ./media/images/bet.png:     1758x155 | 293 WIDTH per frame (6)
+ *  ./media/images/betside.png: 1596x188 | 266 WIDTH per frame (6)
+ *  ./media/images/raven.png:   1626x194 | 271 WIDTH per frame (6)
+ *  ./media/images/spikeball    1917x212 | 213 WIDTH per frame (9)
+*/
